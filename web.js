ffd17c4f3d24e4e660a75e40c74382e0386280cd
Manivannan D Sekaran <Manivannan.D.Sekaran@gmail.com>
2013-07-10 06:57:17 +0530

    Reading the index.html from web.js

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var buffer =fs.readFileSync('index.html');
+	var buf = new Buffer(buffer);
+        response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+ console.log("Listening on " + port);
 });
+
+
